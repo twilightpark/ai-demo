5f59ad0e953ddcf2a0e92c0967c2914a77593d0d
twilightpark <87813985+twilightpark@users.noreply.github.com>
2024-03-26 16:38:54 +0800

    Add files via upload
    增加NLP观点提取

--- src/test/java/com/test/aidemo1/controller/AiController.java
+++ src/test/java/com/test/aidemo1/controller/AiController.java
@@ -1,6 +1,7 @@
 package com.test.aidemo1.controller;
 
 import com.test.aidemo1.utils.AiUtils;
+import com.test.aidemo1.utils.DemoException;
 import org.json.JSONException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,6 +29,18 @@ public class AiController {
     @RequestMapping(value = "/correction",method = RequestMethod.GET)
         public  String NLPCorrect(@RequestParam("text") String text) throws JSONException {
         String res = AiUtils.NLPCorrection(text);
+        return res;
+
+    }
+    @RequestMapping(value = "/views",method = RequestMethod.GET)
+    public  String NLPViews(@RequestParam("text") String text) throws JSONException {
+        String res = AiUtils.NLPViews(text);
+        return res;
+    }
+    @RequestMapping(value = "/vocal",method = RequestMethod.POST)
+    public  String VocalRecognition(@RequestParam("file") MultipartFile file) throws IOException, JSONException, DemoException {
+        String res = AiUtils.vocalRecognition(file);
+//        System.out.println(res);
         return res;
 
     }
